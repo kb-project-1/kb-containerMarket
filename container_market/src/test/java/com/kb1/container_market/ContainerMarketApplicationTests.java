883925f9d63da65fb543b08231ad98f416771408
Doraeki <idh0325@naver.com>
2022-10-13 14:09:13 +0900

    스프링부트 프로젝트,컨트롤러 맵핑

---
+++ container_market/src/test/java/com/kb1/container_market/ContainerMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb1.container_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
